e77eacd0ccba2626d57727714c7cef6b5dc3d800
YP2743 <truongducvt123@gmail.com>
2023-05-22 05:21:01 +0700

    first commit

---
+++ src/test/java/com/foodrec/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodrec.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
