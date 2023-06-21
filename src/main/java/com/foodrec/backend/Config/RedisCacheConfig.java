084255e3c2b84d312e95d4b6fb3ac7627f305ee1
Truong Minh Duc <truongducvt123@gmail.com>
2023-06-21 10:46:09 +0700

    feat: Add redis

--- src/main/java/com/foodrec/backend/Config/RedisCacheConfig.java
+++ src/main/java/com/foodrec/backend/Config/RedisCacheConfig.java
@@ -15,18 +15,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-import java.io.Serializable;
 import java.time.Duration;
 import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
 @AutoConfigureAfter(RedisAutoConfiguration.class)
