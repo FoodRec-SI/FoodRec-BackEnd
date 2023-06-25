a5d956f4ab17566118cce559e02f5a1aeb8363c9
TranNgocNam2 <ngocnamsieuquay@gmail.com>
2023-06-25 09:27:34 +0700

    feat: add view collection details function for collectionAPI

---
+++ src/main/java/com/foodrec/backend/CollectionAPI/query/get_collection_by_id/GetCollectionByIdQueryHandler.java
@@ -0,0 +1,58 @@
+package com.foodrec.backend.CollectionAPI.query.get_collection_by_id;
+
+import an.awesome.pipelinr.Command;
+import com.foodrec.backend.CollectionAPI.dto.CollectionDetailsDTO;
+import com.foodrec.backend.CollectionAPI.entity.Collection;
+import com.foodrec.backend.CollectionAPI.repository.CollectionRepository;
+import com.foodrec.backend.Exception.InvalidDataExceptionHandler;
+import com.foodrec.backend.Exception.NotFoundExceptionHandler;
+import com.foodrec.backend.Exception.UnauthorizedExceptionHandler;
+import com.foodrec.backend.PostAPI.dto.PostDTO;
+import com.foodrec.backend.PostAPI.entity.Post;
+import com.foodrec.backend.PostAPI.entity.PostStatus;
+import com.foodrec.backend.PostAPI.repository.PostRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.*;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class GetCollectionByIdQueryHandler implements Command.Handler<GetCollectionByIdQuery, CollectionDetailsDTO> {
+    private final PostRepository postRepository;
+    private final ModelMapper modelMapper;
+    private final CollectionRepository collectionRepository;
+
+    public GetCollectionByIdQueryHandler(PostRepository postRepository, ModelMapper modelMapper, CollectionRepository collectionRepository) {
+        this.postRepository = postRepository;
+        this.modelMapper = modelMapper;
+        this.collectionRepository = collectionRepository;
+    }
+
+
+    @Override
+    public CollectionDetailsDTO handle(GetCollectionByIdQuery query) {
+        if (query.getPageNumber() < 0 || query.getPageSize() < 1) {
+            throw new InvalidDataExceptionHandler("Invalid data!");
+        }
+        Pageable pageable = PageRequest.of(query.getPageNumber(), query.getPageSize(), Sort.by("recipeName").ascending());
+        Optional<Collection> optionalCollection = collectionRepository.findById(query.getCollectionId());
+        if (optionalCollection.isEmpty()) {
+            throw new NotFoundExceptionHandler("Not found collection");
+        }
+        if (!optionalCollection.get().getUserId().equals(query.getUserId())) {
+            throw new UnauthorizedExceptionHandler("You don't have permission to delete view collection");
+        }
+        Page<Post> postsPage = postRepository.getPostsByPostCollectionsCollectionAndStatus(collectionRepository.findById(query.getCollectionId()).get(), 2, pageable);
+        List<PostDTO> postDTOS = postsPage.getContent().stream().map(post -> {
+            PostDTO postDTO = modelMapper.map(post, PostDTO.class);
+            postDTO.setPostStatus(PostStatus.convertStatusToEnum(post.getStatus()));
+            return postDTO;
+        }).toList();
+        Page<PostDTO> postDTOPage = new PageImpl<>(postDTOS, pageable, postsPage.getTotalElements());
+        CollectionDetailsDTO collectionDetailsDTO = modelMapper.map(optionalCollection.get(), CollectionDetailsDTO.class);
+        collectionDetailsDTO.setPostDTOS(postDTOPage);
+        return collectionDetailsDTO;
+    }
+}
